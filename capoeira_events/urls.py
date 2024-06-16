7e16f7eb3ac3d89019bec26fbf99df64d82adf93
Chris Rodriguez <Crodriguez4443@gmail.com>
2024-06-16 18:17:02 -0400

    added the edit page

--- capoeira_events/urls.py
+++ capoeira_events/urls.py
@@ -1,7 +1,7 @@
 # capoeira_events/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from events.views import HomePageView, CreateEventView, events, event_details, edit_event
+from events.views import HomePageView, CreateEventView, events, event_details, UpdateEventView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -12,8 +12,8 @@ urlpatterns = [
     path('events/', events, name='events'),  # Listing events
     path('accounts/', include('allauth.urls')),
     path('accounts/', include('allauth.socialaccount.urls')),
-    path('event/<int:event_id>/', event_details, name='event_details'),    # Include other URL definitions as needed
-    path('event/<int:event_id>/edit', edit_event, name='edit_event'),
+    path('event/<int:pk>/', event_details, name='event_details'),    # Include other URL definitions as needed
+    path('event/<int:pk>/edit', UpdateEventView.as_view(), name='edit_event'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- events/views.py
+++ events/views.py
@@ -1,9 +1,8 @@
 # events/views.py
-from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.shortcuts import get_object_or_404, render
 from django.views.generic.edit import CreateView
-from django.views.generic import TemplateView
-from django.urls import reverse_lazy
+from django.views.generic import TemplateView, UpdateView
+from django.urls import reverse, reverse_lazy
 from django.shortcuts import render
 from .models import Event
 from .forms import EventForm
@@ -32,51 +31,18 @@ def events(request):
     events = Event.objects.all()  # Retrieve all events from the database
     return render(request, 'events/events.html', {'events': events})
 
-def event_details(request, event_id):
-    event = get_object_or_404(Event, pk=event_id)
+def event_details(request, pk):
+    event = get_object_or_404(Event, pk=pk)
     return render(request, 'events/event_details.html', {'event': event})
 
-def edit_event(request, event_id):
-    """
-    Edit an event by updating its fields with the data from the request.
-    Args:
-        request (HttpRequest): The HTTP request object.
-        event_id (int): The ID of the event to be edited.
-    Returns:
-        HttpResponse: The HTTP response object with the updated event details.
-    """
-    event = get_object_or_404(Event, pk=event_id)
-    if request.method == 'POST':
-        # Update the event's fields with the data from the request
-        event.name = request.POST['name']
-        event.location = request.POST['location']
-        event.address = request.POST['address']
-        event.city = request.POST['city']
-        event.state = request.POST['state']
-        event.zip_code = request.POST['zip_code']
-        event.description = request.POST['description']
-        event.image = request.POST['image']
-        event.category = request.POST['category']
-        event.organizer_name = request.POST['organizer_name']
-        event.organizer_email = request.POST['organizer_email']
-        event.organizer_social_handle = request.POST['organizer_social_handle']
-        event.organizer_social_handle_2 = request.POST['organizer_social_handle_2']
-        event.date = request.POST['date']
-        event.lat = request.POST['lat']
-        event.long = request.POST['long']
-        
-        # Save the updated event to the database
-        event.save()
-        try:
-            db.session.commit()
-        except Exception as e:
-            print("Failed to commit:", e)
-            db.session.rollback()
+class UpdateEventView(UpdateView):
+    model = Event
+    form_class = EventForm
+    template_name = 'events/edit_event.html'
 
-        # Render the event details template with the updated event
-        return render(request, 'events/event_details.html', {'event': event})
-        
-    return render(request, 'events/edit_event.html', {'event': event})
+    def form_valid(self, form):
+        form.instance.organizer = self.request.user  # Set organizer as the current user
+        return super().form_valid(form)
 
-
-
+    def get_success_url(self):
+        return reverse('event_details', kwargs={'pk': self.object.pk})
